2a8ad51b839eccf5713505830861255e4320bd8e
TheVargas-17 <23308060610434@cetis61.edu.mx>
2026-04-20 09:57:17 -0500

    update

---
+++ src/moddels/tareas.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-03-2026 a las 18:10:36
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `registros`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tareas`
+--
+
+CREATE TABLE `tareas` (
+  `id_tarea` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `fecha_limite` date DEFAULT NULL,
+  `hora_limite` time DEFAULT NULL,
+  `estado` enum('pendiente','en_progreso','completada','cancelada') DEFAULT 'pendiente',
+  `clasificacion` enum('personal','trabajo','estudio','hogar','salud') DEFAULT NULL,
+  `prioridad` enum('baja','media','alta') DEFAULT 'media',
+  `completada` tinyint(1) DEFAULT 0,
+  `fecha_completada` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`id_tarea`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  MODIFY `id_tarea` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD CONSTRAINT `tareas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/moddels/views/controllers/UserController.py
@@ -0,0 +1,17 @@
+from models.schemasModel import UsuarioSchema
+from models.UserModel import UsuarioModel
+from pydantic import ValidationError
+
+class AuthController:
+    def __init__(self):
+        self.model = UsuarioModel()
+
+    def registrar_usuario(self, nombre, email, password):
+        try:
+            # Validar datos con el Schema
+            nuevo_usuario = UsuarioSchema(nombre=nombre, email=email, password=password)
+            success = self.model.registrar(nuevo_usuario)
+            return success, "Usuario creado correctamente"
+        except ValidationError as e:
+            # Retorna el primer error de validación encontrado
+            return False, e.errors()[0]['msg']
